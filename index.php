ac1f33913689f279e0ab6651b85b8022b20c8a82
Faisal Ahammed <50483833+faisalahammed044274@users.noreply.github.com>
2020-11-21 02:46:00 +0600

    COMPOSER TEST
    COMPOSER TEST

--- index.php
+++ index.php
@@ -1,49 +1,17 @@
 <?php
-    session_start();
-    if (isset($_SESSION['id'])) {
-        header('Location: dashboard.php');
-    }
+require 'vendor/autoload.php';
 
- require_once './vendor/autoload.php';
- use App\classes\Login;
+use Product\Bangladesh\Laptop;
+use Product\Bangladesh\Mobile;
+use Product\Nepal\Books;
+use Product\Cool\Dell;
+use Food\Usa\Dallas\Chicken;
+use Food\Usa\Sandiago\Beef;
 
- if (isset($_POST['btn'])) {
-    $login =new Login();
-    $login->adminLoginCheck();
- }
-
+$lap = new Laptop;
+$mob = new Mobile;
+$bk = new Books;
+$dl = new Dell;
+$dlas = new Chicken;
+$bf = new Beef;
 ?>
-
-
-
-<style>
-    .form-container{
-        width: 50%;
-        height: 50%;
-        margin: 150px auto;
-        box-shadow: 0 0 3px 3px grey;
-        box-sizing:border-box;
-        padding: 150px;
-
-    }
-
-</style>
-
-<div class="form-container">
-    <form action="" method="POST">
-        <table>
-            <tr>
-                <th>Email Address</th>
-                <td><input type="email" name="email"/></td>
-            </tr>
-            <tr>
-                <th>Password</th>
-                <td><input type="password" name="password"/></td>
-            </tr>
-            <tr>
-                <th></th>
-                <td><input type="submit" name="btn" value="Submit"/></td>
-            </tr>
-        </table>
-    </form>
-</div>
